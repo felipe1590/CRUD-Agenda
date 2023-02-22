0891155bf919c86fc74ace5e6385e24aaf0e8cb0
felipe1590 <felipe.gama.rk@gmail.com>
2023-02-22 18:13:54 -0300

    Codigo

---
+++ CRUD/Form1.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnNovo = New System.Windows.Forms.Button()
+        Me.btnExcluir = New System.Windows.Forms.Button()
+        Me.btnAlterar = New System.Windows.Forms.Button()
+        Me.btnIncluir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgContatos = New System.Windows.Forms.DataGridView()
+        Me.txtNome = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtEndereco = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtTelefone = New System.Windows.Forms.MaskedTextBox()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        CType(Me.dgContatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnNovo
+        '
+        Me.btnNovo.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnNovo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnNovo.Location = New System.Drawing.Point(12, 317)
+        Me.btnNovo.Name = "btnNovo"
+        Me.btnNovo.Size = New System.Drawing.Size(142, 44)
+        Me.btnNovo.TabIndex = 15
+        Me.btnNovo.Text = "Novo"
+        Me.btnNovo.UseVisualStyleBackColor = False
+        '
+        'btnExcluir
+        '
+        Me.btnExcluir.BackColor = System.Drawing.Color.IndianRed
+        Me.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExcluir.Location = New System.Drawing.Point(456, 317)
+        Me.btnExcluir.Name = "btnExcluir"
+        Me.btnExcluir.Size = New System.Drawing.Size(142, 44)
+        Me.btnExcluir.TabIndex = 14
+        Me.btnExcluir.Text = "Excluir"
+        Me.btnExcluir.UseVisualStyleBackColor = False
+        '
+        'btnAlterar
+        '
+        Me.btnAlterar.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.btnAlterar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAlterar.Location = New System.Drawing.Point(308, 317)
+        Me.btnAlterar.Name = "btnAlterar"
+        Me.btnAlterar.Size = New System.Drawing.Size(142, 44)
+        Me.btnAlterar.TabIndex = 13
+        Me.btnAlterar.Text = "Alterar"
+        Me.btnAlterar.UseVisualStyleBackColor = False
+        '
+        'btnIncluir
+        '
+        Me.btnIncluir.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnIncluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnIncluir.Location = New System.Drawing.Point(160, 317)
+        Me.btnIncluir.Name = "btnIncluir"
+        Me.btnIncluir.Size = New System.Drawing.Size(142, 44)
+        Me.btnIncluir.TabIndex = 12
+        Me.btnIncluir.Text = "Incluir"
+        Me.btnIncluir.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(180, 25)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Lista de Contatos"
+        '
+        'dgContatos
+        '
+        Me.dgContatos.AllowUserToAddRows = False
+        Me.dgContatos.AllowUserToDeleteRows = False
+        Me.dgContatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgContatos.Location = New System.Drawing.Point(12, 46)
+        Me.dgContatos.Name = "dgContatos"
+        Me.dgContatos.ReadOnly = True
+        Me.dgContatos.RowHeadersVisible = False
+        Me.dgContatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgContatos.Size = New System.Drawing.Size(301, 249)
+        Me.dgContatos.TabIndex = 10
+        '
+        'txtNome
+        '
+        Me.txtNome.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNome.Location = New System.Drawing.Point(348, 71)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(250, 26)
+        Me.txtNome.TabIndex = 16
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(344, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "Nome"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(344, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 20)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "Endereço"
+        '
+        'txtEndereco
+        '
+        Me.txtEndereco.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEndereco.Location = New System.Drawing.Point(348, 134)
+        Me.txtEndereco.Name = "txtEndereco"
+        Me.txtEndereco.Size = New System.Drawing.Size(250, 26)
+        Me.txtEndereco.TabIndex = 21
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(344, 178)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 20)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "Telefone"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(344, 246)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 20)
+        Me.Label5.TabIndex = 26
+        Me.Label5.Text = "E-mail"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEmail.Location = New System.Drawing.Point(348, 269)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(250, 26)
+        Me.txtEmail.TabIndex = 25
+        '
+        'txtTelefone
+        '
+        Me.txtTelefone.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTelefone.Location = New System.Drawing.Point(348, 201)
+        Me.txtTelefone.Mask = "(00)00000-0000"
+        Me.txtTelefone.Name = "txtTelefone"
+        Me.txtTelefone.Size = New System.Drawing.Size(250, 26)
+        Me.txtTelefone.TabIndex = 27
+        '
+        'txtId
+        '
+        Me.txtId.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtId.Location = New System.Drawing.Point(545, 14)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(53, 26)
+        Me.txtId.TabIndex = 28
+        Me.txtId.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(620, 387)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.txtTelefone)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtEndereco)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNome)
+        Me.Controls.Add(Me.btnNovo)
+        Me.Controls.Add(Me.btnExcluir)
+        Me.Controls.Add(Me.btnAlterar)
+        Me.Controls.Add(Me.btnIncluir)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgContatos)
+        Me.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CRUD - Agenda"
+        CType(Me.dgContatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnNovo As Button
+    Friend WithEvents btnExcluir As Button
+    Friend WithEvents btnAlterar As Button
+    Friend WithEvents btnIncluir As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dgContatos As DataGridView
+    Friend WithEvents txtNome As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtEndereco As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtTelefone As MaskedTextBox
+    Friend WithEvents txtId As TextBox
+End Class
---
+++ CRUD/Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        desabilitaCampos()
+        listarContatos()
+
+        btnIncluir.Enabled = False
+        btnExcluir.Enabled = False
+        btnAlterar.Enabled = False
+    End Sub
+
+    Sub habilitaCampos()
+        txtNome.Enabled = True
+        txtEndereco.Enabled = True
+        txtTelefone.Enabled = True
+        txtEmail.Enabled = True
+    End Sub
+
+    Sub desabilitaCampos()
+        txtNome.Enabled = False
+        txtEndereco.Enabled = False
+        txtTelefone.Enabled = False
+        txtEmail.Enabled = False
+    End Sub
+
+    Sub limpaCampos()
+        txtId.Text = ""
+        txtNome.Text = ""
+        txtEndereco.Text = ""
+        txtTelefone.Text = ""
+        txtEmail.Text = ""
+    End Sub
+
+    Public Sub listarContatos()
+
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter
+
+        Try
+            Abrir()
+            da = New MySqlDataAdapter("SELECT * FROM contatos", con)
+            da.Fill(dt)
+            dgContatos.DataSource = dt
+
+            formataDG()
+        Catch ex As Exception
+            MessageBox.Show("Erro ao listar" + ex.Message)
+            Fechar()
+        End Try
+
+    End Sub
+
+    Sub formataDG()
+        dgContatos.Columns(0).Visible = False
+        dgContatos.Columns(1).HeaderText = "Nome"
+        dgContatos.Columns(2).HeaderText = "Endereço"
+        dgContatos.Columns(3).HeaderText = "Telefone"
+        dgContatos.Columns(4).HeaderText = "Email"
+    End Sub
+
+    Private Sub btnNovo_Click(sender As Object, e As EventArgs) Handles btnNovo.Click
+        habilitaCampos()
+        limpaCampos()
+
+        btnIncluir.Enabled = True
+        btnAlterar.Enabled = False
+        btnExcluir.Enabled = False
+    End Sub
+
+    Private Sub btnIncluir_Click(sender As Object, e As EventArgs) Handles btnIncluir.Click
+
+        If txtNome.Text = "" Then
+            MsgBox("Preenxa o Nome do Contato")
+            txtNome.Focus()
+        ElseIf txtEndereco.Text = "" Then
+            MsgBox("Preenxa o Endereco do Contato")
+            txtEndereco.Focus()
+        ElseIf Not txtTelefone.MaskCompleted Then
+            MsgBox("Preenxa o Telefone do Contato")
+            txtTelefone.Focus()
+        ElseIf txtEmail.Text = "" Then
+            MsgBox("Preenxa o Email do Contato")
+        Else
+            Dim cmd As New MySqlCommand
+            Dim sql As String
+            Try
+                sql = "INSERT INTO contatos(nome, endereco, telefone, email) VALUES ('" & txtNome.Text & "', '" & txtEndereco.Text & "', '" & txtTelefone.Text & "', '" & txtEmail.Text & "')"
+                cmd = New MySqlCommand(sql, con)
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Contato Salvo com Sucesso!")
+
+                desabilitaCampos()
+                limpaCampos()
+                listarContatos()
+
+                btnIncluir.Enabled = False
+
+            Catch ex As Exception
+                MessageBox.Show("Erro ao Salvar" + ex.Message)
+                Fechar()
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub btnAlterar_Click(sender As Object, e As EventArgs) Handles btnAlterar.Click
+        If txtId.Text <> "" Then
+            Dim cmd As New MySqlCommand
+            Dim sql As String
+            Try
+                sql = "UPDATE contatos SET nome ='" & txtNome.Text & "',`endereco`='" & txtEndereco.Text & "',`telefone`='" & txtTelefone.Text & "',`email`='" & txtEmail.Text & "' WHERE id = '" & txtId.Text & "'"
+                cmd = New MySqlCommand(sql, con)
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Contato Alterado com Sucesso!")
+
+                desabilitaCampos()
+                limpaCampos()
+                listarContatos()
+
+                btnAlterar.Enabled = False
+                btnExcluir.Enabled = False
+
+            Catch ex As Exception
+                MessageBox.Show("Erro ao Salvar" + ex.Message)
+                Fechar()
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnExcluir_Click(sender As Object, e As EventArgs) Handles btnExcluir.Click
+        If txtId.Text <> "" Then
+            Dim cmd As New MySqlCommand
+            Dim sql As String
+            Try
+                sql = "DELETE FROM contatos WHERE id = '" & txtId.Text & "'"
+                cmd = New MySqlCommand(sql, con)
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Contato Excluído com Sucesso!")
+
+                desabilitaCampos()
+                limpaCampos()
+                listarContatos()
+
+                btnAlterar.Enabled = False
+                btnExcluir.Enabled = False
+
+            Catch ex As Exception
+                MessageBox.Show("Erro ao Salvar" + ex.Message)
+                Fechar()
+            End Try
+        End If
+    End Sub
+
+    Private Sub dgContatos_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgContatos.CellDoubleClick
+        txtId.Text = dgContatos.CurrentRow.Cells(0).Value
+        txtNome.Text = dgContatos.CurrentRow.Cells(1).Value
+        txtEndereco.Text = dgContatos.CurrentRow.Cells(2).Value
+        txtTelefone.Text = dgContatos.CurrentRow.Cells(3).Value
+        txtEmail.Text = dgContatos.CurrentRow.Cells(4).Value
+
+        habilitaCampos()
+        btnIncluir.Enabled = False
+        btnAlterar.Enabled = True
+        btnExcluir.Enabled = True
+    End Sub
+End Class
